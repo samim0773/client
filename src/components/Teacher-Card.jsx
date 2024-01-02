0a3d33b563c6227ae536827a5a1b9c2664054584
Samim Aktar <samimaktar0773@gmail.com>
2024-01-03 01:02:17 +0530

    Responsive add for teacher card

--- src/components/Teacher-Card.jsx
+++ src/components/Teacher-Card.jsx
@@ -1,55 +1,96 @@
 import "../../public/style/Teacher-card.css";
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-function TeacherCard() {
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//   };
-  return (
-    {data.map((data) =>(
-        <Card style={{ width: "18rem" }}>
-        
-        <Card.Img variant="top" src={data.img} />
-        <Card.Body>
-          <Card.Title>{data.name}</Card.Title>
-          <Card.Text>
-            {data.subject}
-          </Card.Text>
-          <Button variant="primary">View Profile</Button>
-        </Card.Body>
-      </Card>
-        ))} 
-  );
-}
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const data = [
   {
     name: "samim",
     img: "../../public/teachers/principal.jpg",
-    subject: "English"
+    subject: "English",
   },
   {
     name: "samim",
     img: "../../public/teachers/teach2.jpg",
-    subject: "English"
+    subject: "English",
   },
   {
     name: "samim",
     img: "../../public/teachers/teach3.jpg",
-    subject: "English"
+    subject: "English",
   },
   {
     name: "samim",
     img: "../../public/teachers/principal.jpg",
-    subject: "English"
-  }
+    subject: "English",
+  },
+  {
+    name: "samim",
+    img: "../../public/teachers/teach2.jpg",
+    subject: "English",
+  },
+  {
+    name: "samim",
+    img: "../../public/teachers/teach3.jpg",
+    subject: "English",
+  },
 ];
+function TeacherCard() {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <Slider {...settings}>
+          {data.map((data, index) => (
+            <div key={index} className="tech-card">
+              <div className="tch-img">
+                <img src={data.img} alt="" />
+              </div>
+              <div className="tch-info">
+                <h5>{data.name}</h5>
+                <p>{data.subject}</p>
+                <button>View Profile</button>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+}
 
 export default TeacherCard;
